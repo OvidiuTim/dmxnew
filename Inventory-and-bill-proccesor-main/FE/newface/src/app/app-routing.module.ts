db69a81f3855b6a319fac58e570fd00ed23deacb
livboer <boer.livia@gmail.com>
2023-01-19 16:02:34 +0200

    Jan 19
    Magazie and Angajati

--- Inventory-and-bill-proccesor-main/FE/newface/src/app/app-routing.module.ts
+++ Inventory-and-bill-proccesor-main/FE/newface/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MagazieComponent} from './magazie/magazie.component';
+import { AngajatiComponent } from './magazie/angajati/angajati.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path:'magazie',component:MagazieComponent},
+  {path:'angajati',component:AngajatiComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
