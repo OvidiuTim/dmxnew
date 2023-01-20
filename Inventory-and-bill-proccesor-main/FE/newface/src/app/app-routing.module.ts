faa07b40856cefa159d1a12347c7f1abbfc4154b
livboer <boer.livia@gmail.com>
2023-01-20 13:56:51 +0200

    Jan 20
    Sidebar in all pages

--- Inventory-and-bill-proccesor-main/FE/newface/src/app/app-routing.module.ts
+++ Inventory-and-bill-proccesor-main/FE/newface/src/app/app-routing.module.ts
@@ -3,12 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 
 import {MagazieComponent} from './magazie/magazie.component';
 import { AngajatiComponent } from './magazie/angajati/angajati.component';
+import { MaterialeComponent } from './magazie/materiale/materiale.component';
+import { SchelaComponent } from './magazie/schela/schela.component';
+import { UnelteComponent } from './magazie/unelte/unelte.component';
 
 
 
 const routes: Routes = [
   {path:'magazie',component:MagazieComponent},
   {path:'angajati',component:AngajatiComponent},
+  {path:'materiale',component:MaterialeComponent},
+  {path:'schela',component:SchelaComponent},
+  {path:'unelte',component:UnelteComponent},
+  
 ];
 
 @NgModule({
