f3f8e0c8284d62ff537262ceeba30e68b94799b2
livboer <boer.livia@gmail.com>
2023-01-25 10:23:35 +0200

    Jan25
    Administrator > Istoric + copy of Angajati, materiale, unelte, schela

---
+++ Inventory-and-bill-proccesor-main/FE/newface/src/app/administrator/angajati/modal-unelte/modal-unelte.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-unelte',
+  templateUrl: './modal-unelte.component.html',
+  styleUrls: ['./modal-unelte.component.css']
+})
+export class ModalUnelteComponent {
+
+}
