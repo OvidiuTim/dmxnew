a5f22ad36f589031dd03e4803d973c43e6759820
OvidiuTim <88558965+OvidiuTim@users.noreply.github.com>
2025-10-21 15:51:25 +0300

    urls angular and django

--- Inventory-and-bill-proccesor-main/FE/newface/src/app/shared.service.ts
+++ Inventory-and-bill-proccesor-main/FE/newface/src/app/shared.service.ts
@@ -1,286 +1,100 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class SharedService {
-  readonly APIUrl = window.location.origin;
+  // aceeași origine + prefix '/api'
+  private readonly API = (typeof window !== 'undefined' ? window.location.origin : '') + '/api';
 
-  constructor(private http:HttpClient)  { }
+  constructor(private http: HttpClient) {}
 
-  admin:boolean=false;
-  allowthis:boolean=false;
-  selectedUser:any
-  selectedUserPin:any
+  admin = false;
+  allowthis = false;
+  selectedUser: any;
+  selectedUserPin: any;
+  username!: string;
 
-  username !: string;
+  // --- Angajați ---
+  getUsrList(): Observable<any[]>      { return this.http.get<any[]>(`${this.API}/user/`); }
+  addUser(val: any)                    { return this.http.post(`${this.API}/user/`, val); }
+  updateUser(val: any)                 { return this.http.put(`${this.API}/user/`, val); }
+  deleteUser(id: any)                  { return this.http.delete(`${this.API}/user/${id}`); }
 
+  // --- Unelte ---
+  getTolList(): Observable<any[]>      { return this.http.get<any[]>(`${this.API}/tool/`); }
+  addTool(val: any)                    { return this.http.post(`${this.API}/tool/`, val); }
+  updateTool(val: any)                 { return this.http.put(`${this.API}/tool/`, val); }
+  deleteTool(id: any)                  { return this.http.delete(`${this.API}/tool/${id}`); }
 
-    //anagajati api
-    getUsrList():Observable<any[]>{
-      return this.http.get<any[]>(this.APIUrl + '/user/');
-    }  
-  
-    addUser(val:any){
-      return this.http.post(this.APIUrl + '/user/',val);
-    }
-  
-    updateUser(val:any){
-      return this.http.put(this.APIUrl + '/user/',val);
-    }
-  
-    deleteUser(val:any){
-      return this.http.delete(this.APIUrl + '/user/'+val);
-    }
+  // --- Istoric ---
+  getHisList(): Observable<any[]>      { return this.http.get<any[]>(`${this.API}/history/`); }
+  addHistory(val: any)                 { return this.http.post(`${this.API}/history/`, val); }
+  updateHistory(val: any)              { return this.http.put(`${this.API}/history/`, val); }
+  deleteHistory(id: any)               { return this.http.delete(`${this.API}/history/${id}`); }
 
-  //unelte api
-  getTolList():Observable<any[]>{
-    return this.http.get<any[]>(this.APIUrl + '/tool/');
+  // --- Materiale / Consumabile ---
+  getMatList(): Observable<any[]>      { return this.http.get<any[]>(`${this.API}/material/`); }
+  addMaterial(val: any)                { return this.http.post(`${this.API}/material/`, val); }
+  updateMaterial(val: any)             { return this.http.put(`${this.API}/material/`, val); }
+  deleteMaterial(id: any)              { return this.http.delete(`${this.API}/material/${id}`); }
+
+  getConList(): Observable<any[]>      { return this.http.get<any[]>(`${this.API}/consumable/`); }
+  addConsumable(val: any)              { return this.http.post(`${this.API}/consumable/`, val); }
+  updateConsumable(val: any)           { return this.http.put(`${this.API}/consumable/`, val); }
+  deleteConsumable(id: any)            { return this.http.delete(`${this.API}/consumable/${id}`); }
+
+  // --- Schele / Popi / Cofraje / Mijloace fixe (toate cu /api/) ---
+  getCofMetList()                      { return this.http.get<any[]>(`${this.API}/cofrajmetalic/`); }
+  addCofMet(v: any)                    { return this.http.post(`${this.API}/cofrajmetalic/`, v); }
+  updateCofMet(v: any)                 { return this.http.put(`${this.API}/cofrajmetalic/`, v); }
+  deleteCofMet(id: any)                { return this.http.delete(`${this.API}/cofrajmetalic/${id}`); }
+
+  getDokaList()                        { return this.http.get<any[]>(`${this.API}/cofrajtipdoka/`); }
+  addDoka(v: any)                      { return this.http.post(`${this.API}/cofrajtipdoka/`, v); }
+  updateDoka(v: any)                   { return this.http.put(`${this.API}/cofrajtipdoka/`, v); }
+  deleteDoka(id: any)                  { return this.http.delete(`${this.API}/cofrajtipdoka/${id}`); }
+
+  getpopi()                            { return this.http.get<any[]>(`${this.API}/popi/`); }
+  addpopi(v: any)                      { return this.http.post(`${this.API}/popi/`, v); }
+  updatepopi(v: any)                   { return this.http.put(`${this.API}/popi/`, v); }
+  deletepopi(id: any)                  { return this.http.delete(`${this.API}/popi/${id}`); }
+
+  getschelausoara()                    { return this.http.get<any[]>(`${this.API}/schelausoara/`); }
+  addschelausoara(v: any)              { return this.http.post(`${this.API}/schelausoara/`, v); }
+  updateschelausoara(v: any)           { return this.http.put(`${this.API}/schelausoara/`, v); }
+  deleteschelausoara(id: any)          { return this.http.delete(`${this.API}/schelausoara/${id}`); }
+
+  getschelafatada()                    { return this.http.get<any[]>(`${this.API}/schelafatada/`); }
+  addschelafatada(v: any)              { return this.http.post(`${this.API}/schelafatada/`, v); }
+  updateschelafatada(v: any)           { return this.http.put(`${this.API}/schelafatada/`, v); }
+  deleteschelafatada(id: any)          { return this.http.delete(`${this.API}/schelafatada/${id}`); }
+
+  getschelafatadaM()                   { return this.http.get<any[]>(`${this.API}/schelafatadamodulara/`); }
+  addschelafatadaM(v: any)             { return this.http.post(`${this.API}/schelafatadamodulara/`, v); }
+  updateschelafatadaM(v: any)          { return this.http.put(`${this.API}/schelafatadamodulara/`, v); }
+  deleteschelafatadaM(id: any)         { return this.http.delete(`${this.API}/schelafatadamodulara/${id}`); }
+
+  getistoricM()                        { return this.http.get<any[]>(`${this.API}/istoric_schele/`); }
+  addistoricM(v: any)                  { return this.http.post(`${this.API}/istoric_schele/`, v); }
+  updateistoricM(v: any)               { return this.http.put(`${this.API}/istoric_schele/`, v); }
+  deleteistoricM(id: any)              { return this.http.delete(`${this.API}/istoric_schele/${id}`); }
+
+  getmijloace()                        { return this.http.get<any[]>(`${this.API}/mijloacefixe/`); }
+  addmijloace(v: any)                  { return this.http.post(`${this.API}/mijloacefixe/`, v); }
+  updatemijloace(v: any)               { return this.http.put(`${this.API}/mijloacefixe/`, v); }
+  deleteMijloace(id: any)              { return this.http.delete(`${this.API}/mijloacefixe/${id}`); }
+
+  // --- Pontaj ---
+  getAttendanceDay(date?: string)      { return this.http.get(`${this.API}/pontaj/day/`,     { params: date ? { date } : {} }); }
+  getAttendancePresent()               { return this.http.get(`${this.API}/pontaj/present/`); }
+  getAttendanceRange(start: string, end: string) {
+    return this.http.get(`${this.API}/pontaj/range/`, { params: { start, end } });
+  }
+  getAttendanceRangeForUser(start: string, end: string, userId: number) {
+    return this.http.get(`${this.API}/pontaj/range/`, { params: { start, end, user_id: userId } });
   }
 
-  addTool(val:any){
-    return this.http.post(this.APIUrl + '/tool/',val);
-  }
-
-  updateTool(val:any){
-    return this.http.put(this.APIUrl + '/tool/',val);
-  }
-
-
-  deleteTool(val:any){
-    return this.http.delete(this.APIUrl + '/tool/'+val);
-  }
-
-  //istoric api
-  getHisList():Observable<any[]>{
-    return this.http.get<any[]>(this.APIUrl + '/history/');
-  }
-
-  addHistory(val:any){
-    return this.http.post(this.APIUrl + '/history/',val);
-  }
-
-  updateHistory(val:any){
-    return this.http.put(this.APIUrl + '/history/',val);
-  }
-
-
-  deleteHistory(val:any){
-    return this.http.delete(this.APIUrl + '/history/'+val);
-  }
-
-    //material api
-    getMatList():Observable<any[]>{
-      return this.http.get<any[]>(this.APIUrl + '/material/');
-    }
-    
-    addMaterial(val:any){
-      return this.http.post(this.APIUrl + '/material/',val);
-    }
-    
-    updateMaterial(val:any){
-      return this.http.put(this.APIUrl + '/material/',val);
-    }
-    
-    
-    deleteMaterial(val:any){
-      return this.http.delete(this.APIUrl + '/material/'+val);
-      }
-
-
-//consumabile api
-getConList():Observable<any[]>{
-  return this.http.get<any[]>(this.APIUrl + '/consumable/');
-}
-
-addConsumable(val:any){
-  return this.http.post(this.APIUrl + '/consumable/',val);
-}
-
-updateConsumable(val:any){
-  return this.http.put(this.APIUrl + '/consumable/',val);
-}
-
-
-deleteConsumable(val:any){
-  return this.http.delete(this.APIUrl + '/consumable/'+val);
-}
-
-      
-
-
-//cofraj metalic api
-getCofMetList():Observable<any[]>{
-  return this.http.get<any[]>(this.APIUrl + '/cofrajmetalic/');
-}
-
-addCofMet(val:any){
-  return this.http.post(this.APIUrl + '/cofrajmetalic/',val);
-}
-
-updateCofMet(val:any){
-  return this.http.put(this.APIUrl + '/cofrajmetalic/',val);
-}
-
-
-deleteCofMet(val:any){
-  return this.http.delete(this.APIUrl + '/cofrajmetalic/'+val);
-}
-
-
-//cofraj tip doka api
-getDokaList():Observable<any[]>{
-return this.http.get<any[]>(this.APIUrl + '/cofrajtipdoka/');
-}
-
-addDoka(val:any){
-return this.http.post(this.APIUrl + '/cofrajtipdoka/',val);
-}
-
-updateDoka(val:any){
-return this.http.put(this.APIUrl + '/cofrajtipdoka/',val);
-}
-
-
-deleteDoka(val:any){
-return this.http.delete(this.APIUrl + '/cofrajtipdoka/'+val);
-}
-
-
-//popi api
-getpopi():Observable<any[]>{
-return this.http.get<any[]>(this.APIUrl + '/popi/');
-}
-
-addpopi(val:any){
-return this.http.post(this.APIUrl + '/popi/',val);
-}
-
-updatepopi(val:any){
-return this.http.put(this.APIUrl + '/popi/',val);
-}
-
-
-deletepopi(val:any){
-return this.http.delete(this.APIUrl + '/popi/'+val);
-}
-
-//schelausoara api
-getschelausoara():Observable<any[]>{
-return this.http.get<any[]>(this.APIUrl + '/schelausoara/');
-}
-
-addschelausoara(val:any){
-return this.http.post(this.APIUrl + '/schelausoara/',val);
-}
-
-updateschelausoara(val:any){
-return this.http.put(this.APIUrl + '/schelausoara/',val);
-}
-
-
-deleteschelausoara(val:any){
-return this.http.delete(this.APIUrl + '/schelausoara/'+val);
-}
-
-
-//fatada api
-getschelafatada():Observable<any[]>{
-return this.http.get<any[]>(this.APIUrl + '/schelafatada/');
-}
-
-addschelafatada(val:any){
-return this.http.post(this.APIUrl + '/schelafatada/',val);
-}
-
-updateschelafatada(val:any){
-return this.http.put(this.APIUrl + '/schelafatada/',val);
-}
-
-
-deleteschelafatada(val:any){
-return this.http.delete(this.APIUrl + '/schelafatada/'+val);
-}
-
-//s fatada modulara api
-getschelafatadaM():Observable<any[]>{
-return this.http.get<any[]>(this.APIUrl + '/schelafatadamodulara/');
-}
-
-addschelafatadaM(val:any){
-return this.http.post(this.APIUrl + '/schelafatadamodulara/',val);
-}
-
-updateschelafatadaM(val:any){
-return this.http.put(this.APIUrl + '/schelafatadamodulara/',val);
-}
-
-
-deleteschelafatadaM(val:any){
-return this.http.delete(this.APIUrl + '/schelafatadamodulara/'+val);
-}
-
-//s istoric
-getistoricM():Observable<any[]>{
-return this.http.get<any[]>(this.APIUrl + '/istoric_schele/');
-}
-
-addistoricM(val:any){
-return this.http.post(this.APIUrl + '/istoric_schele/',val);
-}
-
-updateistoricM(val:any){
-return this.http.put(this.APIUrl + '/istoric_schele/',val);
-}
-
-
-deleteistoricM(val:any){
-return this.http.delete(this.APIUrl + '/istoric_schele/'+val);
-}
-
-//mijloace api
-getmijloace():Observable<any[]>{
-return this.http.get<any[]>(this.APIUrl + '/mijloacefixe/');
-}
-
-addmijloace(val:any){
-return this.http.post(this.APIUrl + '/mijloacefixe/',val);
-}
-
-updatemijloace(val:any){
-return this.http.put(this.APIUrl + '/mijloacefixe/',val);
-}
-
-
-deleteMijloace(val:any){
-return this.http.delete(this.APIUrl + '/mijloacefixe/'+val);
-}
-
-// --- Pontaj API ---
-
-/** Raport pe o zi: /api/pontaj/day/?date=YYYY-MM-DD  (dacă nu dai date, folosește azi) */
-getAttendanceDay(date?: string){
-  const url = `${this.APIUrl}/api/pontaj/day/` + (date ? `?date=${date}` : '');
-  return this.http.get<any>(url);
-}
-
-/** Cine e IN acum: /api/pontaj/present/ */
-getAttendancePresent(){
-  return this.http.get<any>(`${this.APIUrl}/api/pontaj/present/`);
-}
-
-/** Sumar pe interval: /api/pontaj/range/?start=YYYY-MM-DD&end=YYYY-MM-DD */
-getAttendanceRange(start: string, end: string){
-  const url = `${this.APIUrl}/api/pontaj/range/?start=${start}&end=${end}`;
-  return this.http.get<any>(url);
-}
-
-/** Sumar pe interval pentru un user: /api/pontaj/range/?start=YYYY-MM-DD&end=YYYY-MM-DD&user_id=ID */
-getAttendanceRangeForUser(start: string, end: string, userId: number){
-  const url = `${this.APIUrl}/api/pontaj/range/?start=${start}&end=${end}&user_id=${userId}`;
-  return this.http.get<any>(url);
-}
-
+  // SSE (dacă îl folosești în FE)
+  readonly pontajStreamUrl = `${this.API}/pontaj/stream/`;
 }
--- Inventory-and-bill-proccesor-main/dataAPI/ToolApp/urls.py
+++ Inventory-and-bill-proccesor-main/dataAPI/ToolApp/urls.py
@@ -1,93 +1,103 @@
 from django.urls import path, re_path as url
 from ToolApp import views
-from .views import nfc_scan, attendance_today, attendance_day, attendance_present, attendance_range
-from .views import monitor_pontaj_page, pontaj_stream
-
+from .views import (
+    nfc_scan, attendance_today, attendance_day, attendance_present, attendance_range,
+    monitor_pontaj_page, pontaj_stream
+)
 
 urlpatterns = [
-    # --- NFC / RFID ---
-    path('nfc-tag/', views.nfc_tag_view, name='nfc_tag'),             # dacă îl folosești
-    path('check-nfc-reader/', views.check_nfc_reader, name='check_nfc_reader'),
-    path('rfid/entry/', views.rfid_entry_exit),
-
-    # --- Flux unelte (nou) ---
-    path('tools/issue/', views.issue_tool),     # POST: { tool_serie|tool_fk, user_serie|user_fk, issued_by_serie?, quantity?, note? }
-    path('tools/return/', views.return_tool),   # POST: { tool_serie|tool_fk, user_serie|user_fk, note? }
-    path('tools/status/', views.tools_status),  # GET: statusul curent al tuturor uneltelor
-
-    # --- Endpointuri existente (legacy) ---
-    url(r'^user/$', views.userApi),
-    url(r'^user/([0-9]+)$', views.userApi),
-
-    url(r'^tool/$', views.toolApi),
-    url(r'^tool/([0-9]+)$', views.toolApi),
-
-    url(r'^history/$', views.historyApi),
-    url(r'^history/([0-9]+)$', views.historyApi),
-
-    url(r'^material/$', views.materialApi),
-    url(r'^material/([0-9]+)$', views.materialApi),
-
-    url(r'^consumable/$', views.consumableApi),
-    url(r'^consumable/([0-9]+)$', views.consumableApi),
-
-    url(r'^shed/$', views.shedApi),
-    url(r'^shed/([0-9]+)$', views.shedApi),
-
-    url(r'^workfield/$', views.workfieldApi),
-    url(r'^workfield/([0-9]+)$', views.workfieldApi),
-
-    url(r'^unfunctional/$', views.unfunctionalApi),
-    url(r'^unfunctional/([0-9]+)$', views.unfunctionalApi),
-
-    url(r'^cofrajmetalic/$', views.cofrajmetalicApi),
-    url(r'^cofrajmetalic/([0-9]+)$', views.cofrajmetalicApi),
-
-    url(r'^cofrajtipdoka/$', views.cofrajtipdokaApi),
-    url(r'^cofrajtipdoka/([0-9]+)$', views.cofrajtipdokaApi),
-
-    url(r'^popi/$', views.popiApi),
-    url(r'^popi/([0-9]+)$', views.popiApi),
-
-    url(r'^schelausoara/$', views.schelausoaraApi),
-    url(r'^schelausoara/([0-9]+)$', views.schelausoaraApi),
-
-    url(r'^schelafatada/$', views.schelafatadaApi),
-    url(r'^schelafatada/([0-9]+)$', views.schelafatadaApi),
-
+    # --- Legacy (existente) ---
+    url(r'^user/$', views.userApi),                         url(r'^user/([0-9]+)$', views.userApi),
+    url(r'^tool/$', views.toolApi),                         url(r'^tool/([0-9]+)$', views.toolApi),
+    url(r'^history/$', views.historyApi),                   url(r'^history/([0-9]+)$', views.historyApi),
+    url(r'^material/$', views.materialApi),                 url(r'^material/([0-9]+)$', views.materialApi),
+    url(r'^consumable/$', views.consumableApi),             url(r'^consumable/([0-9]+)$', views.consumableApi),
+    url(r'^shed/$', views.shedApi),                         url(r'^shed/([0-9]+)$', views.shedApi),
+    url(r'^workfield/$', views.workfieldApi),               url(r'^workfield/([0-9]+)$', views.workfieldApi),
+    url(r'^unfunctional/$', views.unfunctionalApi),         url(r'^unfunctional/([0-9]+)$', views.unfunctionalApi),
+    url(r'^cofrajmetalic/$', views.cofrajmetalicApi),       url(r'^cofrajmetalic/([0-9]+)$', views.cofrajmetalicApi),
+    url(r'^cofrajtipdoka/$', views.cofrajtipdokaApi),       url(r'^cofrajtipdoka/([0-9]+)$', views.cofrajtipdokaApi),
+    url(r'^popi/$', views.popiApi),                         url(r'^popi/([0-9]+)$', views.popiApi),
+    url(r'^schelausoara/$', views.schelausoaraApi),         url(r'^schelausoara/([0-9]+)$', views.schelausoaraApi),
+    url(r'^schelafatada/$', views.schelafatadaApi),         url(r'^schelafatada/([0-9]+)$', views.schelafatadaApi),
     url(r'^schelafatadamodulara/$', views.schelafatadamodularaApi),
     url(r'^schelafatadamodulara/([0-9]+)$', views.schelafatadamodularaApi),
+    url(r'^mijloacefixe/$', views.mijloacefixeApi),         url(r'^mijloacefixe/([0-9]+)$', views.mijloacefixeApi),
+    url(r'^combustibil/$', views.combustibilApi),           url(r'^combustibil/([0-9]+)$', views.combustibilApi),
+    url(r'^istoric_schele/$', views.istoricschelaApi),      url(r'^istoric_schele/([0-9]+)$', views.istoricschelaApi),
 
-    url(r'^mijloacefixe/$', views.mijloacefixeApi),
-    url(r'^mijloacefixe/([0-9]+)$', views.mijloacefixeApi),
+    # NFC/Tools helpers (existente)
+    path('nfc-tag/', views.nfc_tag_view, name='nfc_tag'),
+    path('check-nfc-reader/', views.check_nfc_reader, name='check_nfc_reader'),
+    path('rfid/entry/', views.rfid_entry_exit),
+    path('tools/issue/', views.issue_tool),
+    path('tools/return/', views.return_tool),
+    path('tools/status/', views.tools_status),
 
-    url(r'^combustibil/$', views.combustibilApi),
-    url(r'^combustibil/([0-9]+)$', views.combustibilApi),
-
-    url(r'^istoric_schele/$', views.istoricschelaApi),
-    url(r'^istoric_schele/([0-9]+)$', views.istoricschelaApi),
-
-    path('sensor/event/', views.sensor_event),  # ← endpoint pentru Raspberry
-    
-    path("api/nfc/scan/", nfc_scan, name="nfc_scan"), #nfc scan
-    path("api/nfc/scan/", nfc_scan, name="nfc_scan"),
-    
-    
-    path("api/pontaj/day/", attendance_day, name="attendance_day"),
-    path("api/pontaj/present/", attendance_present, name="attendance_present"),
-    path("api/pontaj/range/", attendance_range, name="attendance_range"),
-    
-    
-    path("api/pontaj/today/", attendance_today, name="attendance_today"),
-    
-    
-    
-    
-    
-    # Monitor Pontaj Page
-    path('pontaj/monitor/', monitor_pontaj_page, name='monitor_pontaj'),
+    # --- Pontaj (existente cu /api/) ---
+    path('api/nfc/scan/', nfc_scan, name='nfc_scan'),
+    path('api/pontaj/day/', attendance_day, name='attendance_day'),
+    path('api/pontaj/present/', attendance_present, name='attendance_present'),
+    path('api/pontaj/range/', attendance_range, name='attendance_range'),
+    path('api/pontaj/today/', attendance_today, name='attendance_today'),
     path('api/pontaj/stream/', pontaj_stream, name='pontaj_stream'),
-    
+
+    # Pagina monitor
+    path('pontaj/monitor/', monitor_pontaj_page, name='monitor_pontaj'),
+
+    # Bulk users
     path('api/users/bulk/',  views.users_bulk),
     path('api/users/purge/', views.users_purge),
+
+    # --- Aliasuri noi cu prefix /api/ pentru toate cele de sus (legacy REST) ---
+    path('api/user/', views.userApi),
+    path('api/user/<int:id>', views.userApi),
+
+    path('api/tool/', views.toolApi),
+    path('api/tool/<int:id>', views.toolApi),
+
+    path('api/history/', views.historyApi),
+    path('api/history/<int:id>', views.historyApi),
+
+    path('api/material/', views.materialApi),
+    path('api/material/<int:id>', views.materialApi),
+
+    path('api/consumable/', views.consumableApi),
+    path('api/consumable/<int:id>', views.consumableApi),
+
+    path('api/shed/', views.shedApi),
+    path('api/shed/<int:id>', views.shedApi),
+
+    path('api/workfield/', views.workfieldApi),
+    path('api/workfield/<int:id>', views.workfieldApi),
+
+    path('api/unfunctional/', views.unfunctionalApi),
+    path('api/unfunctional/<int:id>', views.unfunctionalApi),
+
+    path('api/cofrajmetalic/', views.cofrajmetalicApi),
+    path('api/cofrajmetalic/<int:id>', views.cofrajmetalicApi),
+
+    path('api/cofrajtipdoka/', views.cofrajtipdokaApi),
+    path('api/cofrajtipdoka/<int:id>', views.cofrajtipdokaApi),
+
+    path('api/popi/', views.popiApi),
+    path('api/popi/<int:id>', views.popiApi),
+
+    path('api/schelausoara/', views.schelausoaraApi),
+    path('api/schelausoara/<int:id>', views.schelausoaraApi),
+
+    path('api/schelafatada/', views.schelafatadaApi),
+    path('api/schelafatada/<int:id>', views.schelafatadaApi),
+
+    path('api/schelafatadamodulara/', views.schelafatadamodularaApi),
+    path('api/schelafatadamodulara/<int:id>', views.schelafatadamodularaApi),
+
+    path('api/mijloacefixe/', views.mijloacefixeApi),
+    path('api/mijloacefixe/<int:id>', views.mijloacefixeApi),
+
+    path('api/combustibil/', views.combustibilApi),
+    path('api/combustibil/<int:id>', views.combustibilApi),
+
+    path('api/istoric_schele/', views.istoricschelaApi),
+    path('api/istoric_schele/<int:id>', views.istoricschelaApi),
 ]
